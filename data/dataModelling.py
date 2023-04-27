b8e289a64d0c5917862c1982b385edfd44bd894f
Viet Quang <93778662+helloitsurdvq@users.noreply.github.com>
2023-04-27 11:57:59 +0700

    not the fullest data

---
+++ data/dataModelling.py
@@ -0,0 +1,16 @@
+import json
+
+input_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\labelling\\neutral.txt"
+output_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\modelling\\neutral.json"
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    
+data = {'data': []}
+for line in lines:
+    line = line.strip()
+    if line:
+        data['data'].append({'text': line, 'label': 0}) # 0 = positive, 1 = negative, 2 = neutral
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
