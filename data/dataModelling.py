44be763da748c0311b0bba8c90ad828736fa4187
Viet Quang <93778662+helloitsurdvq@users.noreply.github.com>
2023-05-02 11:37:00 +0700

    add more data to modelling (to be continued)

--- data/dataModelling.py
+++ data/dataModelling.py
@@ -1,7 +1,7 @@
 import json
 
-input_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\labelling\\negative.txt"
-output_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\modelling\\negative.json"
+input_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\labelling\\neutral.txt"
+output_file = "C:\\Users\\DELL\\Programming\\python\\StockReview.IntroAI.20222\\data\\modelling\\neutral.json"
 
 with open(input_file, 'r', encoding='utf-8') as f:
     lines = f.readlines()
@@ -10,7 +10,7 @@ data = {'data': []}
 for line in lines:
     line = line.strip()
     if line:
-        data['data'].append({'text': line, 'label': 1}) # 0 = positive, 1 = negative, 2 = neutral
+        data['data'].append({'text': line, 'label': 2}) # 0 = positive, 1 = negative, 2 = neutral
 
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
